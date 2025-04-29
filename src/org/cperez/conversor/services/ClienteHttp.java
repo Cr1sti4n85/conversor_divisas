befeaa17e76b993d22275d0ece0ce7cdc7c62c18
Cristian Perez <cperezlecaros@gmail.com>
2025-04-28 22:45:17 -0400

    Crea ConfigLoader y config.properties para variables de entorno

--- src/org/cperez/conversor/services/ClienteHttp.java
+++ src/org/cperez/conversor/services/ClienteHttp.java
@@ -6,16 +6,22 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.cperez.conversor.ConfigLoader;
+
 public class ClienteHttp {
 
-    String API_KEY = "4580ef046f9b0738fda5a49e";
+    String API_KEY;
     String url;
     HttpClient cliente;
     HttpRequest peticion;
 
-    public ClienteHttp(String origen, String destino, double cantidad){
+    public ClienteHttp(String origen, String destino, double cantidad) {
+
+        // obtencion de API_KEY desde archivo de configuración de variables de entorno
+        API_KEY = ConfigLoader.get("API_KEY");
+
         url = String.format("https://v6.exchangerate-api.com/v6/%s/pair/%s/%s/%.2f",
-        API_KEY, origen, destino, cantidad);
+                API_KEY, origen, destino, cantidad);
         this.cliente = HttpClient.newHttpClient();
         this.peticion = HttpRequest.newBuilder().uri(URI.create(this.url)).build();
 
@@ -23,10 +29,10 @@ public class ClienteHttp {
 
     public String getRequest() throws IOException, InterruptedException {
         HttpResponse<String> respuesta = this.cliente.send(this.peticion,
-        HttpResponse.BodyHandlers.ofString());
+                HttpResponse.BodyHandlers.ofString());
 
         return respuesta.body();
 
     }
-    
+
 }
